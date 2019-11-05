afae590a109091a997de56b2f1684e058ca38ff9
Edward Zhu <hzhu033@uottawa.ca>
2019-11-04 19:14:22 -0500

    Update exercise3.cpp

--- Assignment_3_4/exercise3.cpp
+++ Assignment_3_4/exercise3.cpp
@@ -5,12 +5,16 @@
 #include <iostream>
 #include <fstream>
 #include <stdlib.h>
-
+using namespace std;
 struct bloc{
     char lastname[20],firstname[15],phone[11];
     int age;
 };
 
 int main(){
-
+    cout << "Please enter the file name : ";
+    char filename[21];
+    cin >> filename;
+    ofstream file(filename);
+    
 }
